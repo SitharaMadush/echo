6fecfe7a4fa39f1b1a6a9baffb58047a31df4520
Sithara Madushan <sitharamadushankp@gmail.com>
2024-01-29 07:16:34 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
+    server: {
+        host: true,
+        port: 5175,
+        hmr: {host: '172.19.0.3', protocol: 'ws'},
+      },
+});
